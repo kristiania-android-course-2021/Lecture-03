6d01eb5b3b9cc72dfe55fcac9a87504cdac4ae1c
Imran Baig <imran.baig@vipps.no>
2021-01-22 16:17:30 +0100

    added todos

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/CalculatorActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/CalculatorActivity.kt
@@ -1,15 +1,51 @@
 package com.example.calculatorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.evgenii.jsevaluator.JsEvaluator
+import com.evgenii.jsevaluator.interfaces.JsCallback
+
 
 class CalculatorActivity : AppCompatActivity() {
 
-    var jsEvaluator : JsEvaluator? = null
+    //Todo Question: what is the significance of  "lateinit"?
+    lateinit var jsEvaluator : JsEvaluator
+    lateinit var textViewExpression: TextView
+
+    var buttonEq: Button ? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        buttonEq = findViewById<Button>(R.id.buttonEq)
+        textViewExpression = findViewById<Button>(R.id.textView)
+
+        //Todo: Find out how this project is including thirdparty opensource library JsEvaluator into the project
+        //Todo: Vist lib page: https://github.com/evgenyneu/js-evaluator-for-android to get more help about library
+        jsEvaluator = JsEvaluator(this)
+
+        //Handling of click event for buttonEq
+        buttonEq?.setOnClickListener {
+            evaluate()
+        }
+    }
+
+    private fun evaluate () {
+
+        if( textViewExpression.text.isNotBlank() ) {
+            //Todo note result is coming back in the callback interface
+            jsEvaluator.evaluate(textViewExpression.text as String, object : JsCallback {
+                override fun onResult(result: String) {
+                    textViewExpression.text =  result
+                }
+
+                override fun onError(errorMessage: String) {
+                    textViewExpression.text =  errorMessage
+                }
+            })
+        }
     }
 }
